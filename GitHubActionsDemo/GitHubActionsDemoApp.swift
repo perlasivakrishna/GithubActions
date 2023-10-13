2d187e0d352af728614a7718bb18c37346202d01
perlasivakrishna <sivakrishna.perla@ymail.com>
2023-10-13 17:54:06 +0530

    Initial Commit

---
+++ GitHubActionsDemo/GitHubActionsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubActionsDemoApp.swift
+//  GitHubActionsDemo
+//
+//  Created by Perla Siva Krishna on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubActionsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
